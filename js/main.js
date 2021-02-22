0f1a2fa580b84fc04238446656cccd5faed01809
Giovanni <giovannipor23@gmail.com>
2021-02-22 18:18:06 +0100

    Logica (fino a calcolo costo)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-var nome, cognome, km, eta, offerta, carrozza, codice, costo;
+var nome, km, eta, offerta, carrozza, codice, costo, genera;
+
+const EUROPERKM = 0.21;
+const VALUTA = '€';
+const SCONTO20 = 0.8;
+const SCONTO40 = 0.6;
+
+genera = document.getElementById("genera");
+
+genera.addEventListener ('click',
+
+    function() {
+        nome = document.getElementById("nome").value;
+        console.log(nome);
+
+        km = document.getElementById("km").value;
+        console.log(km);
+
+        eta = document.getElementById("eta").value;   
+        console.log(eta);
+
+        if (eta == "minorenne") {
+            costo = EUROPERKM * km * SCONTO20;
+        }
+
+        else if (eta == "anziano") {
+            costo = EUROPERKM * km * SCONTO40;
+        }
+
+        else {
+            costo = EUROPERKM * km;
+        }
+
+        console.log(costo.toFixed(2)+VALUTA);
+    }
+    
+    )
+    
+
+
