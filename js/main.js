6dcb9cf69745ef8535e5f7006430d3a2300458d6
Giovanni <giovannipor23@gmail.com>
2021-02-22 20:25:52 +0100

    Logica (Genera Biglietto)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var nome, km, eta, offerta, carrozza, codice, costo, genera;
+var nome, km, eta, offerta, carrozza, codice, costo, genera, ticket;
 
 const EUROPERKM = 0.21;
 const VALUTA = '€';
@@ -21,20 +21,41 @@ genera.addEventListener ('click',
 
         if (eta == "minorenne") {
             costo = EUROPERKM * km * SCONTO20;
+            offerta = "Sconto 20%"
         }
 
         else if (eta == "anziano") {
             costo = EUROPERKM * km * SCONTO40;
+            offerta = "Sconto 40%"
         }
 
         else {
             costo = EUROPERKM * km;
+            offerta = "Base"
         }
 
+        ticket = document.getElementById("ticket");
+        ticket.className = "visible";
+
         console.log(costo.toFixed(2)+VALUTA);
+
+        carrozza = Math.floor(Math.random()*10+1);
+        console.log(carrozza);
+
+        codice = Math.floor(Math.random()*100000);
+        console.log(codice);
+
+        document.getElementById("fullname").innerHTML = nome;
+        document.getElementById("offerta").innerHTML = offerta;
+        document.getElementById("carrozza").innerHTML = carrozza;
+        document.getElementById("codice").innerHTML = codice;
+        document.getElementById("costo").innerHTML = costo.toFixed(2)+VALUTA;
     }
     
     )
     
-
-
+/*     annulla = document.getElementById("annulla");
+    annulla.addEventListener ('click',
+        function() {
+        }
+    ) */
